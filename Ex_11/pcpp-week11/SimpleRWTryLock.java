4bfd55cfeb763abfdfd9d330ec409dbfe3afe2cb
Mikkel Thomsen <mikt@itu.dk>
2014-12-19 13:55:03 +0100

    Lock testing

--- Ex_11/pcpp-week11/SimpleRWTryLock.java
+++ Ex_11/pcpp-week11/SimpleRWTryLock.java
@@ -1,7 +1,7 @@
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicReference;
 public class SimpleRWTryLock{
-	private final AtomicReference<Holders> holder = new AtomicReference<Holders>();
+	public final AtomicReference<Holders> holder = new AtomicReference<Holders>();
 	public SimpleRWTryLock(){
 
 	}
@@ -28,7 +28,19 @@ public class SimpleRWTryLock{
 
 	//Ex 11.2.4
 	public void readerUnlock(){
-		
+		final Thread current = Thread.currentThread();
+		if(holder.get() == null){
+			throw new RuntimeException("Not lock holder.");
+		}
+		else if(holder.get() instanceof ReaderList){
+			ReaderList rl = (ReaderList) holder.get();
+			if(!rl.contains(current))
+				throw new RuntimeException("Not lock holder");
+			holder.set(rl.remove(current));
+		}
+		else{
+			throw new RuntimeException("Not lock holder");
+		}
 	}
 	//Ex 11.2.1
 	public boolean writerTryLock(){
@@ -41,8 +53,11 @@ public class SimpleRWTryLock{
 	public void writerUnlock(){
 		final Thread current = Thread.currentThread();
 		Writer w = new Writer(current);
-		if(!holder.compareAndSet(w, null)){
+		if(holder.get().t != current){
 			throw new RuntimeException("Not lock holder");
+		}
+		else{
+			holder.set(null);
 		}
 
 	}
@@ -68,12 +83,83 @@ public class SimpleRWTryLock{
 			this.next = next;
 		}
 
+		public boolean contains(Thread t){
+			if(super.t != t){
+				ReaderList nxt = next;
+				while(nxt.t != null){
+					if(nxt.t == t)
+						return true;
+					nxt = nxt.getNext();
+				}
+				return false;
+			}
+			return true;
+		}
+
+		public ReaderList remove(Thread t){
+			
+			if(super.t != t){
+				ReaderList nxt = next;
+				while(nxt.t != null){
+					if(nxt.getNext().t == t){
+						nxt.setNext(nxt.getNext().getNext());
+						return this;
+					}
+					nxt = nxt.getNext();
+				}
+				throw new RuntimeException("Not lock holder");
+			}
+			return next;
+		}
+
 	}
 
 	private static class Writer extends Holders {
 		public Writer(Thread t){
 			super(t);
 		}
+	}
+}
+
+class TestTryLock {
+	public static void main(String[] args) {
+		testTryLock();
+	}
+
+	private static void testTryLock(){
+		SimpleRWTryLock lock = new SimpleRWTryLock();
+		
+		// assert lock.holder == null;
+		assert lock.writerTryLock() == true;
+		assert lock.writerTryLock() == false;
+		//Make sure lock didn't get freed up just because we tried to take it.
+		assert lock.writerTryLock() == false;
+		//Make sure we can't get reader
+		assert lock.readerTryLock() == false;
+		//No error should be thrown here
+		lock.writerUnlock();
+		boolean thrown = false;
+		try{
+			lock.writerUnlock();
+		}catch(RuntimeException e){
+			thrown = true;
+		}
+		assert thrown == true;
+
+
+		assert lock.readerTryLock() == true;
+		assert lock.readerTryLock() == true;
+		lock.readerUnlock();
+		lock.readerUnlock();
+
+		thrown = false;
+		try{
+			lock.readerUnlock();
+		}catch(RuntimeException e){
+			thrown = true;
+		}
+		assert thrown == true;
+
 	}
 }
 
