b285f7bb39c661167b83cb15ef648e119cd41e60
Anders Jorgensen <abrj@itu.dk>
2014-12-19 12:55:21 +0100

    finished 11.2.3

--- Ex_11/pcpp-week11/SimpleRWTryLock.java
+++ Ex_11/pcpp-week11/SimpleRWTryLock.java
@@ -7,7 +7,23 @@ public class SimpleRWTryLock{
 	}
 	//Ex 11.2.3
 	public boolean readerTryLock(){
-		
+		final Thread current = Thread.currentThread();
+		ReaderList readerList = new ReaderList(current);
+		if(holder.compareAndSet(null, readerList)){
+			return true;
+		}
+		else if(holder.get() instanceof ReaderList){
+			ReaderList rl = (ReaderList) holder.get();
+			while(rl.getNext() != null){
+				rl = rl.getNext();
+			}
+			rl.setNext(readerList);
+			return true;
+		}
+		else{
+			return false;
+		}
+
 	}
 
 	public void readerUnlock(){
@@ -23,27 +39,31 @@ public class SimpleRWTryLock{
 	//Ex 11.2.2
 	public void writerUnlock(){
 		final Thread current = Thread.currentThread();
-		Writer w = new Writer(current)
+		Writer w = new Writer(current);
 		if(!holder.compareAndSet(w, null)){
 			throw new RuntimeException("Not lock holder");
 		}
 
 	}
 	private static abstract class Holders {
-		public final Thread t; 
+		public final Thread t;
+
+		public Holders(Thread t){
+			this.t = t;
+		} 
 	}
 
 	private static class ReaderList extends Holders {
 		private ReaderList next;
 		public ReaderList(Thread t){
-			super.t = t;
+			super(t);
 		}
 
-		public Thread getNext(){
+		public ReaderList getNext(){
 			return next;
 		}
 
-		public void setNext(Thread next){
+		public void setNext(ReaderList next){
 			this.next = next;
 		}
 
@@ -51,7 +71,7 @@ public class SimpleRWTryLock{
 
 	private static class Writer extends Holders {
 		public Writer(Thread t){
-			super.t = t;
+			super(t);
 		}
 	}
 }
